427cedb5740fa12337a7f0f7496366e09295b3ce
smarr <marr.scott@gmail.com>
2016-10-31 21:07:11 -0400

    Initial Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+mport unittest
+from simpleapp import hello_world
+
+class HelloWorldTestCase(unittest.TestCase):
+    """Tests for `simpleapp.py`."""
+
+    def test_is_output_hw(self):
+        """Is the output of your Python Application what you expect?"""
+        self.assertTrue(hello_world() == "Hello World from SMARR!")
+
+if __name__ == '__main__':
+    unittest.main()
